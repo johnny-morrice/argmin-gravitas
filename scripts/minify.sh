35b19771f5a970697ba4a59dc187a8497d0ad658
John Morrice <john.morrice.developer@gmail.com>
2025-03-01 17:06:04 +0000

    Minify images separately

--- scripts/minify.sh
+++ scripts/minify.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 set -e
 
-echo "Size before minification"
+echo "Size before HTML/css/js minification"
 du -sh _site
 
-docker run --rm --platform linux/amd64 -v "$(pwd)/_site":/srv tdewolff/minify -r -o /srv .
+docker run --rm --platform linux/amd64 -v "$(pwd)/_site":/srv tdewolff/minify --exclude "/files/" --exclude "\.svg$" -r -o /srv .
 
-echo "Size after minification"
+echo "Size after HTML/css/js minification"
+du -sh _site
+
+# Build imagemin Dockerfile
+docker build --progress plain -t argmin-gravitas/imagemin -f Dockerfiles/Dockerfile.imagemin .
+
+echo "Size before image minification"
+du -sh _site
+
+docker run --rm -v "$(pwd)/_site":/images argmin-gravitas/imagemin '/images/**/*.{jpg,jpeg,png,gif,svg}' --out-dir=/images
+
+echo "Size after image minification"
 du -sh _site
